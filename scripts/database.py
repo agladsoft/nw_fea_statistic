e5b3e8c78970619841e574af14ad2abc42957f15
uvens <uventus1992@gmail.com>
2024-04-24 00:18:56 +0300

    add : new project

---
+++ scripts/database.py
@@ -0,0 +1,57 @@
+import sys
+from datetime import datetime
+from typing import List, Optional, Tuple, Any
+
+import httpx
+import os
+from clickhouse_connect import get_client
+from clickhouse_connect.driver import Client
+from clickhouse_connect.driver.query import QueryResult
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class MissingEnvironmentVariable(Exception):
+    pass
+
+
+def get_my_env_var(var_name: str, default: Any = None) -> str:
+    try:
+        return os.environ[var_name]
+    except KeyError as e:
+        if default is None:
+            raise MissingEnvironmentVariable(f'{var_name} does not exist') from e
+        else:
+            return str(default)
+
+
+class ClickHouse:
+
+    def __init__(self):
+        self.client: Client = self.connect_db()
+        self.nw_period: str = self.get_nw_period()
+        self.fea_period: str = self.get_fea_period()
+
+    @staticmethod
+    def connect_db() -> Client:
+        try:
+            client: Client = get_client(host=get_my_env_var('HOST'), database=get_my_env_var('DATABASE'),
+                                        username=get_my_env_var('USERNAME_DB'), password=get_my_env_var('PASSWORD'))
+        except httpx.ConnectError as ex_connect:
+            sys.exit(1)
+        return client
+
+    def get_nw_period(self) -> str:
+        query: QueryResult = self.client.query('SELECT month,year from nw_statistic group by month,year')
+        return self.get_information_to_table(query)
+
+    def get_fea_period(self) -> str:
+        query: QueryResult = self.client.query('SELECT month,year from fea_statistic group by month,year')
+        return self.get_information_to_table(query)
+
+    @staticmethod
+    def get_information_to_table(query: QueryResult) -> str:
+        result: List[Tuple[int]] = query.result_rows
+        result = sorted(result, key=lambda x: (-x[1], -x[0]))[0]
+        last_date = datetime(year=result[1], month=result[0], day=1).strftime('%Y-%m')
+        return last_date
---
+++ scripts/main.py
@@ -0,0 +1,171 @@
+import json
+import os
+import sys
+from datetime import datetime
+from typing import Dict, List
+from database import ClickHouse
+
+import pandas as pd
+
+pd.set_option('future.no_silent_downcasting', True)
+
+
+class Statistics:
+    def __init__(self, input_file_path: str, output_folder: str):
+        self.input_file_path: str = input_file_path
+        self.output_folder: str = output_folder
+        self.month_position = {
+            'JAN': {},
+            'FEB': {},
+            'MAR': {},
+            'APR': {},
+            'MAY': {},
+            'JUN': {},
+            'JUL': {},
+            'AUG': {},
+            'SEP': {},
+            'OCT': {},
+            'NOV': {},
+            'DEC': {},
+        }
+        self.total = []
+        self.period = ClickHouse()
+
+    def create_new_key(self, index):
+        key = list(self.month_position)[index]
+        return ' '.join([key, self.month_position[key]['year']])
+
+    def get_start_end_position(self, index):
+        list_month = list(self.month_position)
+        try:
+            start_index = self.month_position[list_month[index]]['column']
+            end_index = self.month_position[list_month[index + 1]]['column']
+        except (IndexError, KeyError):
+            start_index = self.month_position[list_month[index]]['column']
+            end_index = 0
+        if start_index > end_index:
+            end_index = None
+        return start_index, end_index
+
+    def write_to_json(self, direction: str, parsed_data: list) -> None:
+        """
+        Write data to json.
+        """
+        basename: str = os.path.basename(f"{self.input_file_path}_{direction}")
+        output_file_path: str = os.path.join(self.output_folder, f'{basename}.json')
+        with open(f"{output_file_path}", 'w', encoding='utf-8') as f:
+            json.dump(parsed_data, f, ensure_ascii=False, indent=4)
+
+    def delete_total_index(self, end_index, df: pd.DataFrame):
+        if not end_index:
+            df = df.iloc[min(self.total) + 3:]
+            self.total.pop(self.total.index(min(self.total)))
+            return df
+        return df
+
+    @staticmethod
+    def get_month_and_year(data_str: dict):
+        date_obj = datetime.strptime(list(data_str.keys())[0], '%b %Y')
+        month_num = date_obj.month
+        year_num = date_obj.year
+        return month_num, year_num
+
+    def add_new_columns(self, data_year: List[dict]):
+        result = []
+        for data_month in data_year:
+            month, year = self.get_month_and_year(data_month)
+            list_values = list(data_month.values())[0] if list(data_month.values()) else []
+            for values in list_values:
+                data = {}
+                data.update(values)
+                data['month'] = month
+                data['year'] = year
+                data['original_file_name'] = os.path.basename(self.input_file_path)
+                data['original_file_parsed_on'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                result.append(data)
+        return result
+
+    def get_df_month(self, df: pd.DataFrame, start_index, end_index):
+        if not end_index:
+            df_month = df.iloc[:, start_index:]
+        else:
+            df_month = df.iloc[:, start_index: end_index]
+        df_month = df_month.iloc[:min(self.total) + 1]
+
+        if len(df_month.iloc[0]) > 11:
+            df_month = df_month.iloc[:, : 11]
+
+        return df_month
+
+    def get_df_ship_name(self, df: pd.DataFrame):
+        df_ship_name = df.iloc[:min(self.total) + 1, 0]
+        df_ship_name.iloc[1] = 'shipping_line'
+        return df_ship_name
+
+    def parse_data(self, df: pd.DataFrame):
+        result_month = []
+        df = self.get_month_coordinates(df)
+        for index in range(len(self.month_position)):
+            if self.check_month(index):
+                continue
+            start_index, end_index = self.get_start_end_position(index)
+
+            df_month = self.get_df_month(df, start_index, end_index)
+            df_ship_name = self.get_df_ship_name(df)
+            df = self.delete_total_index(end_index, df)
+            df_dict = self.get_information_to_df(df_ship_name, df_month)
+            result_month.append({self.create_new_key(index): df_dict})
+        return result_month
+
+    @staticmethod
+    def get_information_to_df(df_ship_name, df_month):
+        result_df = pd.concat([df_ship_name, df_month], axis=1, ignore_index=True).iloc[1:]
+        result_df: pd.DataFrame = result_df.dropna(how='all')
+        result_df.dropna(axis='columns', how='all', inplace=True)
+        result_df = result_df.reset_index(drop=True)
+        result_df.columns = result_df.iloc[0]
+        result_df = result_df.drop(0)
+        result_df.reset_index(drop=True, inplace=True)
+        result_df.fillna(0, inplace=True)
+        df_dict = result_df.to_dict(orient='records')
+        return df_dict
+
+    def check_month(self, index: int):
+        return not self.month_position[list(self.month_position)[index]]
+
+    def get_month_coordinates(self, df: pd.DataFrame):
+        for index, row in df.iterrows():
+            for column_name, value in row.items():
+                for key in self.month_position:
+                    if value and isinstance(value, str):
+                        if key in value and len(value.split("'")) > 1:
+                            self.month_position[key].update(
+                                {'row': index, 'column': int(column_name.split(':')[-1].strip()),
+                                 'year': value.split("'")[1]})
+                        if value in ('TOTAL',) and int(column_name.split(':')[-1].strip()) == 0:
+                            if index not in self.total:
+                                self.total.append(index)
+        df = df.iloc[:max(self.total) + 1]
+        return df
+
+    def filter_data_to_period(self, dict_data, sheet_name):
+        period = self.period.nw_period if 'NW' in sheet_name else self.period.fea_period
+        period_time = datetime.strptime(period, '%Y-%m')
+        new_date = period_time.replace(year=period_time.year - 1)
+        filter_dict_data = [list(key)[0] for key in dict_data if
+                            datetime.strptime(list(key)[0], '%b %Y') >= new_date]
+        dict_data = [key for key in dict_data if list(key)[0] in filter_dict_data]
+        return dict_data
+
+    def main(self):
+        all_sheets = pd.read_excel(self.input_file_path, sheet_name=None, skiprows=1)
+        for sheet_name in all_sheets:
+            parse_data = self.parse_data(all_sheets[sheet_name])
+            filter_data = self.filter_data_to_period(parse_data, sheet_name)
+            result = self.add_new_columns(filter_data)
+            self.write_to_json(sheet_name.lower(), result)
+
+
+if __name__ == "__main__":
+    report_order: Statistics = Statistics(sys.argv[1], sys.argv[2])
+    report_order.main()
