ac2cd908ac7fe8a4e3816d7045eca0568f17a7a8
uvens <uventus1992@gmail.com>
2024-04-24 09:22:18 +0300

    add : new project

--- bash/main.sh
+++ bash/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xls_path="${XL_IDP_PATH_NW_FEA}"
+xls_path="${XL_IDP_PATH_NW_FEA}/"
 
 done_path="${xls_path}"/done
 if [ ! -d "$done_path" ]; then
@@ -12,7 +12,7 @@ if [ ! -d "$json_path" ]; then
   mkdir "${json_path}"
 fi
 
-find "${xls_path}" -maxdepth 1 -type f \( -name "*.xls*" -or -name "*.XLS*" -or -name "*.xml" \) ! -newermt '3 seconds ago' -print0 | while read -d $'\0' file
+find "${xls_path}" -maxdepth 1 -type f \( -name "*.xls*" -or -name "*.XLS*" \) ! -newermt '3 seconds ago' -print0 | while read -d $'\0' file
 do
 
   if [[ "${file}" == *"error_"* ]];
@@ -20,17 +20,16 @@ do
     continue
   fi
 
-	mime_type=$(file -b --mime-type "$file")
-  echo "'${file} - ${mime_type}'"
-
-	# Will convert csv to json
-	python3 ${XL_IDP_PATH_NW_FEA}/scripts/main.py "${file}" "${json_path}"
+  echo "Will convert XLS* '${file}' to JSON '${json_path}'"
+  python3 "${XL_IDP_ROOT_NW_FEA}/scripts/main.py" "${file}" "${json_path}"
 
   if [ $? -eq 0 ]
-	then
-	  mv "${file}" "${done_path}"
-	else
-	  mv "${file}" "${xls_path}/error_$(basename "${file}")"
-	fi
+    then
+      mv "${file}" "${done_path}"
+    else
+      echo "ERROR during convertion ${file} to json!"
+      mv "${file}" "${xls_path}/error_$(basename "${file}")"
+      continue
+    fi
 
 done
