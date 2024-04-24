2e4c6449457b891e1319c6fdb8ec5e4353a089ee
uvens <uventus1992@gmail.com>
2024-04-24 10:17:32 +0300

    add : new project

--- bash_dir/main.sh
+++ bash_dir/main.sh
@@ -12,7 +12,8 @@ if [ ! -d "$json_path" ]; then
   mkdir "${json_path}"
 fi
 
-find "${xls_path}" -maxdepth 1 -type f \( -name "*.xls*" -or -name "*.XLS*" \) ! -newermt '3 seconds ago' -print0 | while read -d $'\0' file
+IFS=$'\0'
+for file in $(find "${xls_path}" -maxdepth 1 -type f \( -name "*.xls*" -or -name "*.XLS*" \) ! -newermt '3 seconds ago' -print0)
 do
 
   if [[ "${file}" == *"error_"* ]];
